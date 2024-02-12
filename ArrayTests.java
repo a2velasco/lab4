9ae49b95b167c52222b5dbdcf3b9dc122e03bb5f
a2velasco <a2velasco@ucsd.edu>
2024-02-12 14:46:13 -0800

    update

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,12 +4,9 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
-    int[] input2 = {3, 2, 1, 0};
-    ArrayExamples.reverseInPlace(input1);
-    ArrayExamples.reverseInPlace(input2);
-    assertArrayEquals(new int[]{ 3 }, input1);
-    assertArrayEquals(new int[]{0, 1, 2, 3}, input2);
+    int[] input = {0, 1, 2, 3};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{0, 1, 2, 3}, input);
 	}
 
 
